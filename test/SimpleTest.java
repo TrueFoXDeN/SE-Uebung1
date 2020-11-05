5b1ff8e673150a360ebf4d2875143d3c1ea5e5e6
Laurenz <laurenz.dilba@hotmail.de>
2020-11-05 09:55:36 +0100

    Testreihenfolge geändert

--- test/SimpleTest.java
+++ test/SimpleTest.java
@@ -5,17 +5,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SimpleTest {
     public static void main(String[] args) {
         Client c = new Client();
+        System.out.println("Expected: Das Ergebnis der Berechnung: [Übersetzung der Zahl -1 nicht möglich (1.0)]");
+        System.out.print("Actual:   ");
         c.display(-1);
-        System.out.println("Das Ergebnis der Berechnung: [Übersetzung der Zahl -1 nicht möglich (1.0)]");
         System.out.println("-------------------------");
+        System.out.println("Expected: Das Ergebnis der Berechnung: [Übersetzung der Zahl 11 nicht möglich (1.0)]");
+        System.out.print("Actual:   ");
         c.display(11);
-        System.out.println("Das Ergebnis der Berechnung: [Übersetzung der Zahl 11 nicht möglich (1.0)]");
         System.out.println("-------------------------");
+        System.out.println("Expected: Das Ergebnis der Berechnung: [Übersetzung der Zahl 0 nicht möglich (1.0)]");
+        System.out.print("Actual:   ");
         c.display(0);
-        System.out.println("Das Ergebnis der Berechnung: [Übersetzung der Zahl 0 nicht möglich (1.0)]");
         System.out.println("-------------------------");
+        System.out.println("Expected: Das Ergebnis der Berechnung: [sieben]");
+        System.out.print("Actual:   ");
         c.display(7);
-        System.out.println("Das Ergebnis der Berechnung: [sieben]");
+
 
     }
 }
